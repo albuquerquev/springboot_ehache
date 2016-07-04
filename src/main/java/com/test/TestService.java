8fa3092c4c818d0e185d421876a24ac4f1963abc
Vijay Albuquerque <misc@techvj.com>
2016-07-05 07:25:19 +1000

    Added TestService - Candidate For Caching.

---
+++ src/main/java/com/test/TestService.java
@@ -0,0 +1,5 @@
+package com.test;
+
+public interface TestService {
+	public String getString();
+}
---
+++ src/main/java/com/test/TestServiceImpl.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class TestServiceImpl implements TestService {
+	@Override
+	public String getString() {
+		return "Hello World!";
+	}
+}
